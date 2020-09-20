9dace19999928a9f392516ed704477f22a0ed51f
viethoang99 <viet.mta.hp@gmail.com>
2020-09-20 23:45:39 +0700

    Sua nhi phan thanh hex

--- MaHoaDES/Form1.cs
+++ MaHoaDES/Form1.cs
@@ -20,6 +20,7 @@ namespace MaHoaDES
         //int MaHoaOrGiaMa = 1;//chế độ(=1:mã hoá, =-1:giải mã)
         //RoundKey key;
         DES_process des;
+       
         private void btnMaHoa_Click(object sender, EventArgs e)
         {
             des = new DES_process();
@@ -28,7 +29,10 @@ namespace MaHoaDES
                 txbKetQua.Text = "";
                 txbQuaTrinh.Text = "";
                 string cipher = des.MaHoa(txbBanRo.Text, txbKhoa.Text, 1,txbQuaTrinh);
-                txbKetQua.Text = cipher;
+                var hex = string.Join("",
+                                        Enumerable.Range(0, cipher.Length / 8)
+                                        .Select(i => Convert.ToByte(cipher.Substring(i * 8, 8), 2).ToString("X2")));
+                txbKetQua.Text = hex;
             }
             else
                 MessageBox.Show("Khoá không hợp lệ");
